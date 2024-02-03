4cee4f85d079071cf7177c10d07ceaf18a03a406
Tikaram Mardi <tikarammardi007@gmail.com>
2024-02-03 17:42:54 +0530

    asking user to enter host for scanning

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module portscanner
+module github.com/tikarammardi/portscanner
 
 go 1.21
+
+require github.com/spf13/pflag v1.0.5
--- main.go
+++ main.go
@@ -1,12 +1,40 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"portscanner/app"
+	"github.com/spf13/pflag"
+	"github.com/tikarammardi/portscanner/app"
+	"os"
+	"strings"
 )
 
 func main() {
-	host := "localhost"
+	var host string
+	pflag.StringVar(&host, "host", "", "Specify the host to scan for open ports.")
+	fmt.Println("Before parsing", host)
+	pflag.Parse()
+
+	fmt.Println("After parsing", host)
+
+	if host == "" {
+		fmt.Println("Please specify a host to scan.")
+		scanner := bufio.NewScanner(os.Stdin)
+		if scanner.Scan() {
+			host = strings.TrimSpace(scanner.Text())
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Println("Error reading from input:", err)
+			os.Exit(1)
+		}
+
+		if host == "" {
+			fmt.Println("No host specified.")
+			os.Exit(1)
+		}
+	}
+
 	startPort := 1
 	endPort := 65535
 

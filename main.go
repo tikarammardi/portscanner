296d38b1abfeacc7dfb83a2b29010a17e05db54c
Tikaram Mardi <tikarammardi007@gmail.com>
2024-02-03 16:41:49 +0530

    Main file included.

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	host := "localhost"
+
+	startPort := 1
+	endPort := 65535
+
+	fmt.Println("Scanning ports on", host, "from", startPort, "to", endPort)
+	// Implement the port scanner here
+
+}
